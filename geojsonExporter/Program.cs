1569f61bd0123deae48c1ab12d10971fe97c45d0
jarle pedersen <jarle.pedersen@norconsult.com>
2018-01-04 11:26:10 +0100

    use netTopologySuite and reproject

--- geojsonExporter/Program.cs
+++ geojsonExporter/Program.cs
@@ -1,18 +1,34 @@
-﻿using Dapper;
+﻿using System;
+using Dapper;
 using System.Collections.Generic;
 using System.Data;
 using System.Data.SqlClient;
+using System.IO;
 using System.Linq;
+using GeoAPI.CoordinateSystems.Transformations;
+using GeoAPI.Geometries;
+using NetTopologySuite.Geometries;
+using NetTopologySuite.IO;
 using Newtonsoft.Json;
+using ProjNet.CoordinateSystems;
+
 
 namespace geojsonExporter
 {
     public class Program
     {
         private const string ConnStr = "server=.;database=NiNCore_norsk;trusted_connection=yes";
+        private static readonly GeoJsonWriter Writer = new GeoJsonWriter();
+        private static readonly WKBReader Reader = new WKBReader();
+        private static ICoordinateTransformation _trans;
 
         public static void Main(string[] args)
         {
+            var ctFact = new ProjNet.CoordinateSystems.Transformations.CoordinateTransformationFactory();
+
+            _trans = ctFact.CreateFromCoordinateSystems(ProjectedCoordinateSystem.WGS84_UTM(33, true), GeographicCoordinateSystem.WGS84);
+
+
             var naturområdeTyper = ReadAll<NaturområdeType>("NaturområdeType");
             var beskrivelsesVariabler = ReadAll<Beskrivelsesvariabel>("Beskrivelsesvariabel");
             var naturområder = ReadAll<NaturområdeJson>("Naturområde");
@@ -29,7 +45,7 @@ namespace geojsonExporter
                 if (naturområdeTyper.All(n => n.Naturområde_id != naturområdeJson.Id) &&
                     beskrivelsesVariabler.All(b => b.Naturområde_id != naturområdeJson.Id)) continue;
 
-                if (i > 100) break;
+                //if (i > 100) break;
 
                 naturområdeJson.Geometry = ConvertToJson(geometries.First(g => g.Id == naturområdeJson.Id));
 
@@ -45,7 +61,9 @@ namespace geojsonExporter
 
             }
 
-            var json = JsonConvert.SerializeObject(root).Replace("\"[[[", "[[[").Replace("]]]\"", "]]]");
+            var json = JsonConvert.SerializeObject(root).Replace("\"[[[", "[[[").Replace("]]]}\"", "]]]");
+
+            File.WriteAllText(@"c:\tmp\naturomrader3.json", json);
 
 
         }
@@ -57,7 +75,7 @@ namespace geojsonExporter
 
             foreach (var r in rødlistekategoris.Where(r => r.naturområde_id == naturområdeJson.Id))
             {
-                naturområdeJson.Properties["RØ_" + kategoriSet.First(k => k.Id == r.kategori_id).verdi] = 0;
+                naturområdeJson.Properties["RØ_" + kategoriSet.First(k => k.Id == r.kategori_id).verdi] = "0";
             }
             
 
@@ -70,7 +88,7 @@ namespace geojsonExporter
             {
                 foreach (var naturområdeType in naturområdeTyper.Where(n => n.Naturområde_id == naturområdeJson.Id))
                 {
-                    naturområdeJson.Properties[naturområdeType.Kode] = naturområdeType.Andel;
+                    naturområdeJson.Properties[naturområdeType.Kode] = naturområdeType.Andel.ToString();
                 }
             }
         }
@@ -78,42 +96,81 @@ namespace geojsonExporter
         private static void AddBeskrivelsesVariabler(IReadOnlyCollection<Beskrivelsesvariabel> beskrivelsesVariabler,
             NaturområdeJson naturområdeJson)
         {
+            // Hvis starter med tall, splitt på underscore, ellers på bindestrek
             if (beskrivelsesVariabler.All(b => b.Naturområde_id != naturområdeJson.Id)) return;
             {
                 foreach (var beskrivelsesVariabel in beskrivelsesVariabler.Where(b =>
                     b.Naturområde_id == naturområdeJson.Id).Select(b => b.Kode).ToList())
                 {
-                    if (beskrivelsesVariabel.Contains(","))
+                    foreach (var codePart in beskrivelsesVariabel.Split(","))
                     {
-                        foreach (var codePart in beskrivelsesVariabel.Split(","))
+                        if (char.IsNumber(codePart.ToCharArray()[0]))
                         {
-                            naturområdeJson.Properties[codePart.Trim()] = 0;
+                            if (codePart.Contains("XY"))
+                            {
+                                naturområdeJson.Properties[codePart.Split("XY")[0]] = "XY";
+                            }
+                            else
+                            {
+                                var parts = codePart.Trim().Split('_');
+                                naturområdeJson.Properties[parts[0]] = parts[1];
+                            }
                         }
+
+                        else
+                        {
+                            var parts = codePart.Trim().Split('-');
+                            try
+                            {
+                                naturområdeJson.Properties[parts[0]] = parts[1];
+                            }
+                            catch 
+                            {
+                                Console.WriteLine("Beskrivelsesvariabel matcher ikke mønster: " + beskrivelsesVariabel + "for naturområde med Id " + naturområdeJson.Id);
+                            }
+                        }
+
                     }
-                    else naturområdeJson.Properties[beskrivelsesVariabel] = 0;
                 }
             }
         }
 
         private static Dictionary<string, string> ConvertToJson(Geometry geometry)
         {
+            var geom = (Polygon)Reader.Read(geometry.WKB);
+
+            TransformRing(geom.Boundary);
+
+            foreach (var geomInteriorRing in geom.InteriorRings) TransformRing(geomInteriorRing);
+            
             return new Dictionary<string, string>
             {
                 {"type", geometry.GeometryType},
-                {"coordinates", geometry.Coordinates}
+                {"coordinates", "[[[" + Writer.Write(geom).Split("[[[")[1]}
             };
+        }
+
+        private static void TransformRing(IGeometry ring)
+        {
+            for (var i = 0; i < ring.NumGeometries; i++)
+            {
+                TransformLinearRing(ring.GetGeometryN(i));
+            }
+        }
+
+        private static void TransformLinearRing(IGeometry linearRing)
+        {
+            foreach (var c in linearRing.Coordinates)
+            {
+                c.CoordinateValue = _trans.MathTransform.Transform(c);
+            }
         }
 
         private static List<Geometry> ReadGeometries()
         {
             using (IDbConnection db = new SqlConnection(ConnStr))
             {
-                //return db.Query<Geometry>("SELECT id, geometri.STGeometryType() as GeometryType, geometri.ToString() as WKT FROM Naturområde").ToList();
-
-                return db.Query<Geometry>(
-                        @"SELECT id, geometri.STGeometryType() as GeometryType,(CASE geometri.STGeometryType() WHEN 'POINT' THEN REPLACE(REPLACE(REPLACE(REPLACE(geometri.ToString(), UPPER(geometri.STGeometryType()) + ' ', ''), '(', '['), ')', ']'), ' ', ',')" +
-                        " ELSE '[' + REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(geometri.ToString(), UPPER(geometri.STGeometryType()) + ' ', ''), '(', '['), ')', ']'), '], ', ']],['),', ','],['),' ',', ') + ']' END) as Coordinates FROM Naturområde")
-                    .ToList();
+                return db.Query<Geometry>("SELECT id, geometri.STGeometryType() as GeometryType, geometri.STAsBinary() as WKB FROM Naturområde").ToList();
             }
         }
 
@@ -159,15 +216,15 @@ namespace geojsonExporter
         public string type = "Feature";
         public Dictionary<string, string> Geometry = new Dictionary<string, string>();
         public int Id { get; set; }
-        public Dictionary<string, int> Properties = new Dictionary<string, int>();
-        public crs crs = new crs();
+        public Dictionary<string, string> Properties = new Dictionary<string, string>();
+        //public crs crs = new crs();
     }
 
     public class Geometry
     {
         public int Id { get; set; }
         public string GeometryType { get; set; }
-        public string Coordinates { get; set; }
+        public byte[] WKB { get; set; }
     }
 
     public class crs
